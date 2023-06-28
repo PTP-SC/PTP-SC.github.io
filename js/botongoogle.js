28c979fe8415c9c107674d4127917fb40796bc03
PTP-SC <137132071+PTP-SC@users.noreply.github.com>
2023-06-28 06:52:05 +0000

    asds

--- js/botongoogle.js
+++ js/botongoogle.js
@@ -2,4 +2,17 @@ function googleSignIn() {
     const provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithRedirect(provider);
   }
-  
+  firebase.auth().getRedirectResult()
+  .then((result) => {
+    // Se obtuvo el resultado del inicio de sesión redirigido
+    const user = result.user;
+    console.log('Usuario ha iniciado sesión:', user);
+    // Realiza las acciones adicionales que desees después del inicio de sesión exitoso
+  })
+  .catch((error) => {
+    // Ocurrió un error durante el inicio de sesión redirigido
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log('Error en el inicio de sesión redirigido:', errorCode, errorMessage);
+    // Maneja el error o muestra un mensaje de error al usuario
+  });
